3f27d717a5c2b8f45180ff32f6ec8b49a1173866
Waithaka <jwaithaka746@gmail.com>
2021-11-27 19:52:25 +0300

    adding database connection

--- server.js
+++ server.js
@@ -13,10 +13,8 @@ const db = knex({
   // connect to your own database here:
   client: "pg",
   connection: {
-    host: "postgresql-spherical-44125",
-    user: "johnwaithaka",
-    password: "",
-    database: "smart-brain",
+    host: process.env.DATABASE_URL,
+    ssl: true,
   },
 });
 
