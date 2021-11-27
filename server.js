de5ad0ffc2944f41e41eb98bcbc2a47976727d06
Waithaka <jwaithaka746@gmail.com>
2021-11-27 19:39:22 +0300

    updating database

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ const db = knex({
   // connect to your own database here:
   client: "pg",
   connection: {
-    host: "127.0.0.1",
+    host: "postgresql-spherical-44125",
     user: "johnwaithaka",
     password: "",
     database: "smart-brain",
