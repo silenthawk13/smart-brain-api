c679fbcf6d6e9221ba2513bafcd715576b6e81ee
Waithaka <jwaithaka746@gmail.com>
2021-11-28 08:59:16 +0300

    updated ssl

--- server.js
+++ server.js
@@ -8,14 +8,15 @@ const register = require("./controllers/register");
 const signin = require("./controllers/signin");
 const profile = require("./controllers/profile");
 const image = require("./controllers/image");
-process.env.NODE_TLS_REJECT_UNAUTHORIZED = 1;
 
 const db = knex({
   // connect to your own database here:
   client: "pg",
   connection: {
     connectionString: process.env.DATABASE_URL,
-    ssl: true,
+    ssl: {
+      rejectUnauthorized: false,
+    },
   },
 });
 
