7bd140fe68120778689cb3dea0cf7e2c2a49160e
Waithaka <jwaithaka746@gmail.com>
2021-11-30 07:15:32 +0300

    logged the error returned

--- controllers/register.js
+++ controllers/register.js
@@ -21,12 +21,12 @@ const handleRegister = (req, res, db, bcrypt) => {
             joined: new Date(),
           })
           .then((user) => {
-            console.log(err);
             res.json(user[0]);
           });
       })
       .then(trx.commit)
       .catch(trx.rollback);
+    console.log(err);
   }).catch((err) => res.status(400).json("unable to register"));
 };
 
