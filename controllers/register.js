2cd603c3f8907b44bd2be7f9576eda3334e95d4c
Waithaka <jwaithaka746@gmail.com>
2021-11-30 06:52:45 +0300

    logged the error

--- controllers/register.js
+++ controllers/register.js
@@ -1,36 +1,35 @@
 const handleRegister = (req, res, db, bcrypt) => {
   const { email, name, password } = req.body;
   if (!email || !name || !password) {
-    return res.status(400).json('incorrect form submission');
+    return res.status(400).json("incorrect form submission");
   }
   const hash = bcrypt.hashSync(password);
-    db.transaction(trx => {
-      trx.insert({
+  db.transaction((trx) => {
+    trx
+      .insert({
         hash: hash,
-        email: email
+        email: email,
       })
-      .into('login')
-      .returning('email')
-      .then(loginEmail => {
-        return trx('users')
-          .returning('*')
+      .into("login")
+      .returning("email")
+      .then((loginEmail) => {
+        return trx("users")
+          .returning("*")
           .insert({
             email: loginEmail[0],
             name: name,
-            joined: new Date()
+            joined: new Date(),
           })
-          .then(user => {
+          .then((user) => {
+            console.log(err);
             res.json(user[0]);
-          })
+          });
       })
       .then(trx.commit)
-      .catch(trx.rollback)
-    })
-    .catch(err => res.status(400).json('unable to register'))
-}
-
-module.exports = {
-  handleRegister: handleRegister
+      .catch(trx.rollback);
+  }).catch((err) => res.status(400).json("unable to register"));
 };
 
-
+module.exports = {
+  handleRegister: handleRegister,
+};
